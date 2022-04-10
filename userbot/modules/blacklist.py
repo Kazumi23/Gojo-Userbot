03eb1bd7322edf0609ab50c243342ecd7260a22e
Cloder07 <gojouserbot@gmail.com>
2022-04-10 16:11:43 +0000

    Gojo-Userbot: auto fixes
    Signed-off-by: Cloder07 <gojouserbot@gmail.com>

--- userbot/modules/blacklist.py
+++ userbot/modules/blacklist.py
@@ -11,7 +11,7 @@ import re
 
 import userbot.modules.sql_helper.blacklist_sql as sql
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, bot
+from userbot import CMD_HELP
 from userbot.utils import edit_or_reply, poci_cmd, pocong_handler
 
 
