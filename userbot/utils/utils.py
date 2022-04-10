ccba71ed88bc33a249066d60759422fec77103be
Cloder07 <gojouserbot@gmail.com>
2022-04-10 13:48:00 +0000

    Gojo-Userbot: auto fixes
    Signed-off-by: Cloder07 <gojouserbot@gmail.com>

--- userbot/utils/utils.py
+++ userbot/utils/utils.py
@@ -9,7 +9,6 @@ from pathlib import Path
 from random import randint
 
 import heroku3
-from telethon.tl.functions.channels import CreateChannelRequest
 from telethon.tl.functions.contacts import UnblockRequest
 
 from userbot import (
