f4d6854c5d0cb895887f3807fe8a89fe4377b496
Gojo Satoru <92911145+Cloder07@users.noreply.github.com>
2022-04-12 04:19:46 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -205,9 +205,9 @@ CMD_HELP.update(
     {
         "update": f"**Plugin : **`update`\
         \n\n  •  **Syntax :** `{cmd}update`\
-        \n  •  **Function : **Untuk Melihat Pembaruan Terbaru PocongUserbot.\
+        \n  •  **Function : **Untuk Melihat Pembaruan Terbaru Gojo-Userbot.\
         \n\n  •  **Syntax :** `{cmd}update deploy`\
-        \n  •  **Function : **Untuk MengUpdate Fitur Terbaru Dari PocongUserbot.\
+        \n  •  **Function : **Untuk MengUpdate Fitur Terbaru Dari Gojo-Userbot.\
     "
     }
 )
